bc0d79de8867ec59f8170b39a6e29a29a5ee6607
Yaya-Cout <yaya.cout@free.fr>
2022-09-16 18:36:59 +0200

    [routes/user] Remove pseudo logging

--- routes/user/main.js
+++ routes/user/main.js
@@ -2,21 +2,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 
-// We need to ensure that pseudo is a valid pseudo (non null, non empty, etc.)
-router.param('pseudo', (req, res, next, pseudo) => {
-    // Check if the pseudo is valid
-    if (pseudo === undefined || pseudo === null || pseudo === '') {
-        // The pseudo is not valid, return an error
-        return res.status(401).json({
-            error: 'Invalid pseudo'
-        });
-    }
-
-    // The pseudo is valid, continue
-    next();
-});
-
-
 // Import the route handlers
 router.use('/:pseudo', require('./by-pseudo/main'));
 
